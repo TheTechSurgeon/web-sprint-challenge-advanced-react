85560154ef43fd75006c131cff4432f5e244e237
strengthofthemountain <strengthofthemountain@gmail.com>
2020-05-08 13:59:53 -0400

    not done

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,45 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (handleSubmitCallback, validateCallback, initialValues) => {
+    const [form, setForm] = useState(initialValues); 
+
+    const [submitting, setSubmitting] = useState(false); 
+   
+    
+
+    
+    const handleChange = e => {
+        const { name, value } = e.target; 
+        setForm(state => {
+            return { ...state, [name]: value };
+        });
+    };
+    
+    const handleSubmit = async e => {
+        setSubmitting(true);
+      
+
+        if (Object.keys(err).length === 0) {
+            
+            setSubmitting(false);
+            console.log('no errors.');
+        } else {
+            setSubmitting(false);
+            
+        }
+    };
+
+    return {
+        handleChange,
+        
+        handleSubmit,
+        setForm,
+        form,
+        errors,
+        submitting,
+    };
+};
+export default useForm;
+
+
